16a1e055eb4cd957d0b7b6b08009e29dc5c4f202
Prahlad Panthi <prahlad1231@gmail.com>
2020-05-10 08:39:46 +0545

    add linear search and binary search

---
+++ searching/Search.java
@@ -0,0 +1,21 @@
+// applies only to integer numbers 
+
+class Search {
+
+	public int linearSearch(int[] arr,int element) {
+		for (int i = 0; i < arr.length; ++i) {
+			if (i == element) return i;
+		}
+		return -1;
+	}
+
+	public int binarySearch(int[] arr, int low, int high, int element) {
+		int mid = (low + high) / 2;
+		if (arr[mid] == element) return mid;
+		else if (arr[mid] < element) 
+			return binarySearch(arr, mid+1, high, element);
+		else if (arr[mid] > element) 
+			return binarySearch(arr, low, mid-1, element);
+		return -1;
+	}
+}
